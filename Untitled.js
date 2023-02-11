9406c18534084bcf52cc91ee9e4cbdfa39459185
sjamesadeshina <sjamesadeshina@gmail.com>
2023-02-11 01:04:22 +0100

    riches assignment

---
+++ Untitled.js
@@ -0,0 +1,93 @@
+// function squareNumber(num){
+//     let squareNumber = num**2
+//     return squareNumber
+// }
+// console.log(squareNumber(8))
+
+// 0! = 1
+// 1! = 1
+
+// 6!
+// 6*5*4*3*2*1 = 720
+
+// function factorial(n) {
+//     if (n === 0 || n === 1) {
+//       return 1;
+//     } else {
+//       return n * factorial(n - 1);
+//     }
+// }
+//   let result = factorial(6);
+//  console.log(result);
+
+// const squareNumber = (n)=> {
+//     return n*n
+// }
+// let value = squareNumber(4)
+// console.log(value)
+
+// const names = ["John", "Peace", "Samuel", "Joy", "Bob", "Sandra"]
+// let sortedName = names.sort();
+// console.log(sortedName);
+
+// let reversedName = names.reverse();
+// console.log(reversedName)
+
+// const scores = [21,41,11,7,9,15,100,40,51]
+// console.log(scores.sort())
+
+// scores.sort((a,b)=>{
+//     return a-b
+// })
+// console.log(scores)
+
+// const names = ["john", "fred", "rich", "sam", "james"]
+// let len = names.length
+// len
+// let firstName = names[3]
+// firstName
+
+// for(let index = 0; index < names.length; index++){
+//     let name = names[index]
+//     console.log(name); 
+// }
+
+// names.forEach(name =>{
+//     console.log(name);
+// });
+
+// let newNames = names.map(name =>{
+//     return name
+// })
+// console.log(newNames)
+
+// let scores = [2,4,6,8,10]
+// let newScore = scores.map(score =>{
+//     return score *2
+// })
+// newScore
+
+// let scores = [2,4,6,8,10,12,14,16,7,8,14,13,16]
+// let validScores = []
+// for(let index = 0; index < scores.length; index++) {
+//     const score = scores[index];
+//     if(score >= 10){
+//         validScores.push(score)
+//     }
+// }
+// validScores 
+
+// let correctScores = scores.filter(score =>{
+//     return score >= 10
+// })
+// correctScores
+
+// let scores = [2,4,6,8,10,12,14,16,7,8,14,13,16]
+
+// let total = scores.reduce((acc, score) =>{
+//     return acc + score
+// })
+// total
+
+
+
